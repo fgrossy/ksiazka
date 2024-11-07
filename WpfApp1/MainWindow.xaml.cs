127aa4cceccdb113455ccee692a5b21e4886deef
Filip <113941244+fgrossy@users.noreply.github.com>
2024-11-07 08:54:06 +0100

    Update MainWindow.xaml.cs

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,71 +1,34 @@
-﻿using System;
 using System.Windows;
 
-namespace WpfApp1
+namespace YourNamespace
 {
     public partial class MainWindow : Window
     {
-        // Stawka za kilometr
-        private double reimburseRate = 0.39;
+        Elephant lloyd = new Elephant { Name = "Lloyd", EarSize = 40 };
+        Elephant lucinda = new Elephant { Name = "Lucinda", EarSize = 33 };
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void buttonLloyd_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                // Pobranie wartości z pól tekstowych
-                double startingMileage = Convert.ToDouble(textBoxStartingMileage.Text);
-                double endingMileage = Convert.ToDouble(textBoxEndingMileage.Text);
-
-                // Walidacja - czy początkowy stan licznika jest mniejszy od końcowego
-                if (startingMileage > endingMileage)
-                {
-                    MessageBox.Show("Początkowy stan licznika musi być mniejszy niż końcowy stan licznika.", "Błąd");
-                    return;
-                }
-
-                // Obliczanie odległości i kwoty do zwrotu
-                double milesTraveled = endingMileage - startingMileage;
-                double amountOwed = milesTraveled * reimburseRate;
-
-                // Wyświetlanie wyniku
-                labelResult.Text = amountOwed.ToString("C");
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Wprowadź prawidłowe liczby.", "Błąd");
-            }
+            lloyd.WhoAmI();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void buttonLucinda_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                // Pobranie wartości z pól tekstowych
-                double startingMileage = Convert.ToDouble(textBoxStartingMileage.Text);
-                double endingMileage = Convert.ToDouble(textBoxEndingMileage.Text);
+            lucinda.WhoAmI();
+        }
 
-                // Walidacja - czy początkowy stan licznika jest mniejszy od końcowego
-                if (startingMileage > endingMileage)
-                {
-                    MessageBox.Show("Początkowy stan licznika musi być mniejszy niż końcowy stan licznika.", "Błąd");
-                    return;
-                }
+        private void buttonSwap_Click(object sender, RoutedEventArgs e)
+        {
+            Elephant temp = lloyd;
+            lloyd = lucinda;
+            lucinda = temp;
 
-                // Obliczanie odległości i kwoty do zwrotu
-                double milesTraveled = endingMileage - startingMileage;
-                labelResult.Text = milesTraveled.ToString();
-
-
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Wprowadź prawidłowe liczby.", "Błąd");
-            }
+            MessageBox.Show("Objects swapped!");
         }
     }
 }
