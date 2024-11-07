b3998445998253b3095a7a4bd5f99938b151242b
Filip <113941244+fgrossy@users.noreply.github.com>
2024-11-07 12:05:42 +0100

    Update MainWindow.xaml.cs

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,71 +1,28 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfApp4
 {
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
-        // Stawka za kilometr
-        private double reimburseRate = 0.39;
-
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                // Pobranie wartości z pól tekstowych
-                double startingMileage = Convert.ToDouble(textBoxStartingMileage.Text);
-                double endingMileage = Convert.ToDouble(textBoxEndingMileage.Text);
-
-                // Walidacja - czy początkowy stan licznika jest mniejszy od końcowego
-                if (startingMileage > endingMileage)
-                {
-                    MessageBox.Show("Początkowy stan licznika musi być mniejszy niż końcowy stan licznika.", "Błąd");
-                    return;
-                }
-
-                // Obliczanie odległości i kwoty do zwrotu
-                double milesTraveled = endingMileage - startingMileage;
-                double amountOwed = milesTraveled * reimburseRate;
-
-                // Wyświetlanie wyniku
-                labelResult.Text = amountOwed.ToString("C");
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Wprowadź prawidłowe liczby.", "Błąd");
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Pobranie wartości z pól tekstowych
-                double startingMileage = Convert.ToDouble(textBoxStartingMileage.Text);
-                double endingMileage = Convert.ToDouble(textBoxEndingMileage.Text);
-
-                // Walidacja - czy początkowy stan licznika jest mniejszy od końcowego
-                if (startingMileage > endingMileage)
-                {
-                    MessageBox.Show("Początkowy stan licznika musi być mniejszy niż końcowy stan licznika.", "Błąd");
-                    return;
-                }
-
-                // Obliczanie odległości i kwoty do zwrotu
-                double milesTraveled = endingMileage - startingMileage;
-                labelResult.Text = milesTraveled.ToString();
-
-
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Wprowadź prawidłowe liczby.", "Błąd");
-            }
         }
     }
 }
